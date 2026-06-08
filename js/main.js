2a3e871db1c34e5035c34911c50715f87711d0ee
Lucas <lucas@wiltzius.fr>
2026-06-08 12:30:26 +0200

    Add du JS pour le menu hamburger

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 // On attend que la page soit complètement chargée
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   console.log('La page est chargée !');
 
   const sections = document.querySelectorAll('section');
 
-  const observer = new IntersectionObserver(function(entries) {
-    entries.forEach(function(entry) {
+  const observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
       if (entry.isIntersecting) {
         entry.target.classList.add('visible');
       }
     });
   });
 
-  sections.forEach(function(section) {
+  sections.forEach(function (section) {
     observer.observe(section);
   });
+});
+
+// Menu hamburger
+const hamburger = document.getElementById('hamburger');
+const navLiens = document.getElementById('nav-liens');
+
+hamburger.addEventListener('click', function () {
+  navLiens.classList.toggle('ouvert');
 });
